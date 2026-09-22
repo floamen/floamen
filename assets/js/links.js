c2979416a104536e452b1f707d7a9fac0418bb20
floamen <floamen.design@gmail.com>
2026-09-22 20:44:49 +0200

    add-etsy

--- assets/js/links.js
+++ assets/js/links.js
@@ -1,6 +1,6 @@
 // Edit links here. Leave Etsy and installationVideo empty until ready.
 window.FLOAMEN_LINKS = {
-  etsy: "",
+  etsy: "https://floamen.etsy.com",
   instagram: "https://www.instagram.com/floamen_design/",
   tiktok: "https://www.tiktok.com/@floamen",
   youtube: "https://www.youtube.com/@floamen",
